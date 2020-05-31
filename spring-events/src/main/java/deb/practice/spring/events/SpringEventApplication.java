478ae8cd1ae1344a615c9163bb5207be91e1fd9a
Deb <debmalya.biswas@darts-ip.com>
2020-05-31 16:13:30 +0530

    Initial repository build

---
+++ spring-events/src/main/java/deb/practice/spring/events/SpringEventApplication.java
@@ -0,0 +1,16 @@
+package deb.practice.spring.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Deb
+ * Date : 31/05/2020.
+ */
+@SpringBootApplication
+public class SpringEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEventApplication.class, args);
+    }
+}
